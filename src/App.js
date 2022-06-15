3e0d5754022d0344777a27606e3cadd6c18be6f9
Jordan <jordanrob709@gmail.com>
2022-06-16 00:04:46 +0300

    setup React Router links and nav | created Dashboard, Login, Register pages as well as the Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
