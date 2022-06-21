1f73bdc59e2addb56afa178579a8f08544fa737b
Jordan <jordanrob709@gmail.com>
2022-06-21 17:00:47 +0300

    created GoalItem component and added it to dashboard so as to display goals

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { GoalForm } from '../components/GoalForm'
 import Spinner from '../components/Spinner'
 import { getGoals, reset } from '../features/goals/goalsSlice'
+import GoalItem from '../components/GoalItem'
 
 const Dashboard = () => {
 
@@ -42,6 +43,16 @@ const Dashboard = () => {
         <p>Goals Dashboard</p>
       </section>
       <GoalForm />
+
+      <section className="content">
+        { goals.length > 0 ? (
+          <div className="goals">
+            {goals.map(goal => 
+              <GoalItem key={goal._id} goal={goal}/>
+            )}
+          </div>
+        ) : (<h3> You have not set any goals </h3>) }
+      </section>
     </>
     
   )
